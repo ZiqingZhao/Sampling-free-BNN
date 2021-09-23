47d84680a948e97bcb83df5a87f989649099ed18
ZiqingZhao <desared@163.com>
2021-09-23 15:34:41 +0000

    feat: comparison of dense and diagonal H

--- hessian/classification_dense.py
+++ hessian/classification_dense.py
@@ -1,6 +1,8 @@
 from re import X
 import sys
 import os
+
+from torch._C import _has_torch_function_variadic
 os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 
 from numpy.core.function_base import add_newdoc
@@ -58,22 +60,22 @@ test_set = datasets.MNIST(root=data_path,
                                         train=False,
                                         transform=transforms.ToTensor(),
                                         download=True)
-test_loader = DataLoader(test_set, batch_size=1)
+test_loader = DataLoader(test_set, batch_size=256)
 
 N = 200
 std = 0.1
 
 # Train the model
-net = BaseNet_15k()
+net = BaseNet_750()
 net.weight_init(std)
 if device == 'cuda': 
     net.to(torch.device('cuda'))
 get_nb_parameters(net)
 criterion = torch.nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-train(net, device, train_loader, criterion, optimizer, epochs=10)
-# save(net, model_path + 'BaseNet_15k.dat')
-# load(net, model_path + 'BaseNet_750.dat')
+# train(net, device, train_loader, criterion, optimizer, epochs=10)
+# save(net, model_path + 'BaseNet_750.dat')
+load(net, model_path + 'BaseNet_750.dat')
 
 # run on the testset
 sgd_predictions, sgd_labels = eval(net, device, test_loader)
@@ -81,15 +83,13 @@ acc = accuracy(sgd_predictions, sgd_labels)
 
 # update likelihood FIM
 H = None
-for images, labels in tqdm(test_loader):
+for images, labels in tqdm(train_loader):
     logits = net(images.to(device))
     dist = torch.distributions.Categorical(logits=logits)
-    # A rank-1 Kronecker factored FiM approximation.
     labels = dist.sample()
     loss = criterion(logits, labels)
     net.zero_grad()
     loss.backward()
-            
     grads = []
     for layer in list(net.modules())[1:]:
         for p in layer.parameters():    
@@ -99,18 +99,25 @@ for images, labels in tqdm(test_loader):
     H_loss = J_loss.t() @ J_loss
     H_loss.requires_grad = False
     H = H_loss if H == None else H + H_loss
+            
+H = H.cpu()/len(train_loader) 
 
-H = H.cpu()/len(test_loader) 
+diag = torch.diag(std * torch.ones(H.shape[0]))
+H_inv = torch.linalg.pinv(N * H + diag).cpu()
 
-sum_diag = torch.diag(H).abs().sum().item()
-sum_non_diag = torch.abs(H-torch.diag(torch.diag(H))).sum()
-print(f"sum of diagonal: {sum_diag:.2f}")
-print(f"sum of non-diagonal: {sum_non_diag:.2f}")
+min = H_inv.abs().min().item()
+max = H_inv.abs().max().item()
+H_norm = (H_inv.abs() - min) / (max-min)
 
+PIL_image = Image.fromarray(np.uint8(255*torch.sqrt(H_norm).numpy())).convert('RGB')
+PIL_image.save(result_path+'750/H_inv_750_dense.png')
 
+'''
 min = H.abs().min().item()
 max = H.abs().max().item()
 H_norm = (H.abs() - min) / (max-min)
 
-PIL_image = Image.fromarray(np.uint8(255*torch.sqrt(H_norm[:3000,:3000]).numpy())).convert('RGB')
+PIL_image = Image.fromarray(np.uint8(255*torch.sqrt(H_norm).numpy())).convert('RGB')
 PIL_image.save(result_path+'750/H_750_dense.png')
+
+'''
--- sampling_free/classification/classification_ll_diagonal.py
+++ sampling_free/classification/classification_ll_diagonal.py
@@ -1,17 +1,18 @@
 from re import X
 import sys
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 from numpy.core.function_base import add_newdoc
 current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
+parent = os.path.dirname(os.path.dirname(current))
 sys.path.append(parent)
 
 # Standard imports
 import numpy as np
 from tqdm import tqdm
 from matplotlib import pyplot as plt
+from PIL import Image, ImageOps  
 
 import torch
 import torch.nn as nn
@@ -21,11 +22,10 @@ from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 
 # From the repository
-from models.wrapper import BaseNet
 from models.curvatures import Diagonal, BlockDiagonal, KFAC, EFB, INF
-from models.utilities import calibration_curve
-from models import plot
-
+from models.utilities import *
+from models.plot import *
+from models.wrapper import *
 
 def gradient(y, x, grad_outputs=None):
     """Compute dy/dx @ grad_outputs"""
@@ -47,116 +47,139 @@ def jacobian(y, x, device):
         jac[i,:] = torch.flatten(gradient(y, x, grad_outputs))
     return jac
 
-if __name__ == '__main__':
     
-    models_dir = 'theta'
-    results_dir = 'results'
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    # load and normalize MNIST
-    new_mirror = 'https://ossci-datasets.s3.amazonaws.com/mnist'
-    datasets.MNIST.resources = [
-        ('/'.join([new_mirror, url.split('/')[-1]]), md5)
-        for url, md5 in datasets.MNIST.resources
-    ]
+# file path
+parent = os.path.dirname(os.path.dirname(current))
+data_path = parent + "/data/"
+model_path = parent + "/theta/"
+result_path = parent + "/results/Hessian/"
 
-    train_set = datasets.MNIST(root="../data",
-                                           train=True,
-                                           transform=transforms.ToTensor(),
-                                           download=True)
-    train_loader = DataLoader(train_set, batch_size=32)
+# choose the device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+torch.manual_seed(42)
+if device == 'cuda':
+    torch.cuda.manual_seed(42) 
 
-    # And some for evaluating/testing
-    test_set = datasets.MNIST(root="../data",
-                                          train=False,
-                                          transform=transforms.ToTensor(),
-                                          download=True)
-    test_loader = DataLoader(test_set, batch_size=1)
+# load and normalize MNIST
+new_mirror = 'https://ossci-datasets.s3.amazonaws.com/mnist'
+datasets.MNIST.resources = [
+    ('/'.join([new_mirror, url.split('/')[-1]]), md5)
+    for url, md5 in datasets.MNIST.resources
+]
 
-    # Train the model
-    net = BaseNet(lr=1e-3, epoch=3, batch_size=32, device=device)
-    #net.load(models_dir + '/theta_best.dat')
-    criterion = nn.CrossEntropyLoss().to(device)
-    net.train(train_loader, criterion)
-    sgd_predictions, sgd_labels = net.eval(test_loader)
+train_set = datasets.MNIST(root=data_path,
+                                        train=True,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+train_loader = DataLoader(train_set, batch_size=32)
+
+# And some for evaluating/testing
+test_set = datasets.MNIST(root=data_path,
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+test_loader = DataLoader(test_set, batch_size=256)
+
+
+N = 200
+std = 0.1
+
+# Train the model
+net = BaseNet_750()
+net.weight_init(std)
+if device == 'cuda': 
+    net.to(torch.device('cuda'))
+get_nb_parameters(net)
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# train(net, device, train_loader, criterion, optimizer, epochs=10)
+# save(net, model_path + 'BaseNet_750.dat')
+load(net, model_path + 'BaseNet_750.dat')
+
+# run on the testset
+sgd_predictions, sgd_labels = eval(net, device, test_loader)
+acc = accuracy(sgd_predictions, sgd_labels)
+
+# compute the diagonal FiM
+diag = Diagonal(net)
+
+for images, labels in tqdm(train_loader):
+    logits = net(images.to(device))
+    dist = torch.distributions.Categorical(logits=logits)
+    # A rank-1 Kronecker factored FiM approximation.
+    labels = dist.sample()
+    loss = criterion(logits, labels)
+    net.zero_grad()
+    loss.backward()
+    diag.update(batch_size=images.size(0))
+
+
+# inversion of H
+estimator = diag
+estimator.invert(std, N)
+
+h = []
+for i,layer in enumerate(list(estimator.model.modules())[1:]):
+    if layer in estimator.state:
+        H_i = estimator.inv_state[layer]
+        h.append(torch.flatten(H_i))
+H = torch.cat(h, dim=0)
+
+H_inv = torch.diag(H).cpu()
+min = H_inv.abs().min().item()
+max = H_inv.abs().max().item()
+H_norm = (H_inv.abs() - min) / (max-min)
+
+PIL_image = Image.fromarray(np.uint8(255*torch.sqrt(H_norm).numpy())).convert('RGB')
+PIL_image.save(result_path+'750/H_inv_750_diag.png')
+
+targets = torch.Tensor()
+diag_prediction = torch.Tensor().to(device)
+diag_uncertainty = torch.Tensor().to(device)
+
+for images,labels in tqdm(test_loader):
+    # prediction mean, equals to the MAP output 
+    pred_mean = torch.nn.functional.softmax(net.model(images.to(device)) ,dim=1)        
+    # compute prediction variance  
+    grad_outputs = torch.zeros_like(pred_mean)
+    idx  = np.argmax(pred_mean.cpu().detach().numpy(), axis=1)
+    grad_outputs[:,idx] = 1
+    g = []
+    for p in net.model.parameters():    
+        g.append(torch.flatten(gradient(pred_mean, p, grad_outputs=grad_outputs)))
+    J = torch.cat(g, dim=0).unsqueeze(0) 
+    pred_std = torch.abs(J * H * J).sum()
+    del J
+    torch.cuda.empty_cache()
+    const = 2*np.e*np.pi 
+    entropy = 0.5 * torch.log2(const * pred_std.unsqueeze(0))
+    # ground truth
+    targets = torch.cat([targets, labels])  
+    # prediction, mean value of the gaussian distribution
+    diag_prediction = torch.cat([diag_prediction, pred_mean]) 
+    diag_uncertainty = torch.cat([diag_uncertainty, entropy]) 
     
-    print(f"MAP Accuracy: {100 * np.mean(np.argmax(sgd_predictions.cpu().numpy(), axis=1) == sgd_labels.numpy()):.2f}%")
+print(f"Diagonal Accuracy: {100 * np.mean(np.argmax(diag_prediction.cpu().detach().numpy(), axis=1) == targets.numpy()):.2f}%")
+print(f"Mean Diagonal Entropy: {diag_uncertainty.mean()}")
+# kfac entropy: -0.64
 
-    # compute the Kronecker factored FiM
-    diag = Diagonal(net.model)
-    #kfac.load(models_dir + '/kfac.dat')
-    
-    for images, labels in tqdm(train_loader):
-        logits = net.model(images.to(device))
-        dist = torch.distributions.Categorical(logits=logits)
-        # A rank-1 Kronecker factored FiM approximation.
-        labels = dist.sample()
-        loss = criterion(logits, labels)
-        net.model.zero_grad()
-        loss.backward()
-        diag.update(batch_size=images.size(0))
-    
-
-    # inversion of H and Q
-    estimator = diag
-    add = 1
-    multiply = 200
-    estimator.invert(add, multiply)
-
-    
-    h = []
-    for i,layer in enumerate(list(estimator.model.modules())[1:]):
-        if layer in estimator.state:
-            H_i = estimator.inv_state[layer]
-            h.append(torch.flatten(H_i))
-    H = torch.cat(h, dim=0)
-    
-    targets = torch.Tensor()
-    diag_prediction = torch.Tensor().to(device)
-    diag_uncertainty = torch.Tensor().to(device)
-
-    for images,labels in tqdm(test_loader):
-        # prediction mean, equals to the MAP output 
-        pred_mean = torch.nn.functional.softmax(net.model(images.to(device)) ,dim=1)        
-        # compute prediction variance  
-        grad_outputs = torch.zeros_like(pred_mean)
-        idx  = np.argmax(pred_mean.cpu().detach().numpy(), axis=1)
-        grad_outputs[:,idx] = 1
-        g = []
-        for p in net.model.parameters():    
-            g.append(torch.flatten(gradient(pred_mean, p, grad_outputs=grad_outputs)))
-        J = torch.cat(g, dim=0).unsqueeze(0) 
-        pred_std = torch.abs(J * H * J).sum()
-        del J
-        torch.cuda.empty_cache()
-        const = 2*np.e*np.pi 
-        entropy = 0.5 * torch.log2(const * pred_std.unsqueeze(0))
-        # ground truth
-        targets = torch.cat([targets, labels])  
-        # prediction, mean value of the gaussian distribution
-        diag_prediction = torch.cat([diag_prediction, pred_mean]) 
-        diag_uncertainty = torch.cat([diag_uncertainty, entropy]) 
-        
-    print(f"Diagonal Accuracy: {100 * np.mean(np.argmax(diag_prediction.cpu().detach().numpy(), axis=1) == targets.numpy()):.2f}%")
-    print(f"Mean Diagonal Entropy: {diag_uncertainty.mean()}")
-    # kfac entropy: -0.64
-
-    res_uncertainty = torch.Tensor().to(device)
-    for i in tqdm(range(10000)):
-        noise = torch.randn_like(images)
-        pred_mean = torch.nn.functional.softmax(net.model(noise.to(device)) ,dim=1)        
-        # compute prediction variance  
-        grad_outputs = torch.zeros_like(pred_mean)
-        idx  = np.argmax(pred_mean.cpu().detach().numpy(), axis=1)
-        grad_outputs[:,idx] = 1
-        g = []
-        for p in net.model.parameters():    
-            g.append(torch.flatten(gradient(pred_mean, p, grad_outputs=grad_outputs)))
-        J = torch.cat(g, dim=0).unsqueeze(0) 
-        pred_std = torch.abs(J * H * J).sum()
-        del J
-        torch.cuda.empty_cache()
-        const = 2*np.e*np.pi 
-        entropy = 0.5 * torch.log2(const * pred_std.unsqueeze(0))
-        res_uncertainty = torch.cat([res_uncertainty, entropy]) 
-    print(f"Mean Noise Entropy: {res_uncertainty.mean()}")
-    # noise entropy: 2.86
+res_uncertainty = torch.Tensor().to(device)
+for i in tqdm(range(10000)):
+    noise = torch.randn_like(images)
+    pred_mean = torch.nn.functional.softmax(net.model(noise.to(device)) ,dim=1)        
+    # compute prediction variance  
+    grad_outputs = torch.zeros_like(pred_mean)
+    idx  = np.argmax(pred_mean.cpu().detach().numpy(), axis=1)
+    grad_outputs[:,idx] = 1
+    g = []
+    for p in net.model.parameters():    
+        g.append(torch.flatten(gradient(pred_mean, p, grad_outputs=grad_outputs)))
+    J = torch.cat(g, dim=0).unsqueeze(0) 
+    pred_std = torch.abs(J * H * J).sum()
+    del J
+    torch.cuda.empty_cache()
+    const = 2*np.e*np.pi 
+    entropy = 0.5 * torch.log2(const * pred_std.unsqueeze(0))
+    res_uncertainty = torch.cat([res_uncertainty, entropy]) 
+print(f"Mean Noise Entropy: {res_uncertainty.mean()}")
+# noise entropy: 2.86
